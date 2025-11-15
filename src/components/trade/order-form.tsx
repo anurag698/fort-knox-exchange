35ba27e103d5a02f3a2050992b871c4bbe03e0a3
ANURAG Kumar <anuragsonujha7@gmail.com>
2025-11-15 17:54:34 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/components/trade/order-form.tsx
+++ src/components/trade/order-form.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useForm } from "react";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { useUser, useFirestore, errorEmitter, FirestorePermissionError } from "@/firebase";
